d8ac4c132d10e50429d0a54b9ad70ca3b90b77d5
Serghey Samokhval <samsonskiff@gmail.com>
2022-04-08 16:20:38 +0300

    add: references on organisations and socials

--- frontend/src/components/Footer/DesktopFooter/index.tsx
+++ frontend/src/components/Footer/DesktopFooter/index.tsx
@@ -32,17 +32,17 @@ export const Footer = () => (
         <div className={styles.socialsWrapper}>
           <span className={styles.socialsTitle}>Join Us on Socials</span>
           <div className={styles.socials}>
-            <Link href="/">
+            <Link href="https://discord.gg/rkKFRjns">
               <a>
                 <Icon iconName="discord" />
               </a>
             </Link>
-            <Link href="/">
+            <Link href="https://twitter.com/diveboard?s=11&t=pF2sFevv7v1darAHt1qDtQ">
               <a>
                 <Icon iconName="twitter" />
               </a>
             </Link>
-            <Link href="/">
+            <Link href="https://github.com/Diveboard">
               <a>
                 <Icon iconName="github" />
               </a>
--- frontend/src/components/PageBlocks/HomePageBlocks/Guest/CollaborationBlock/index.tsx
+++ frontend/src/components/PageBlocks/HomePageBlocks/Guest/CollaborationBlock/index.tsx
@@ -23,22 +23,22 @@ export const CollaborationBlock = () => (
       </p>
 
       <div className={styles.organisations}>
-        <Link href="/">
+        <Link href="https://www.daneurope.org/en/home">
           <a>
             <Icon iconName="DAN" width={133} height={40} />
           </a>
         </Link>
-        <Link href="/">
+        <Link href="https://obis.org/">
           <a>
             <Icon iconName="BIS" width={270} height={40} />
           </a>
         </Link>
-        <Link href="/">
+        <Link href="https://www.gbif.org/">
           <a>
             <Icon iconName="GBIF" width={197} height={40} />
           </a>
         </Link>
-        <Link href="/">
+        <Link href="https://www.marinespecies.org/">
           <a>
             <Icon iconName="WoRMS" width={126} height={40} />
           </a>
