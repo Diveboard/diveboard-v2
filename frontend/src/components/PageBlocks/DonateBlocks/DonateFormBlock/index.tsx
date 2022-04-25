287cbab37809267a6a3a46fda69e2a9a5da7416b
alyona-shirpal <shirpalalona@gmail.com>
2022-04-25 11:00:54 +0300

    added files to donatation

--- frontend/src/components/PageBlocks/DonateBlocks/DonateFormBlock/index.tsx
+++ frontend/src/components/PageBlocks/DonateBlocks/DonateFormBlock/index.tsx
@@ -3,7 +3,11 @@ import styles from './styles.module.scss';
 
 export const DonateFormBlock: FC = () => (
     <div className={styles.wrapper}>
+         <div className={styles.title}>
+             <h2> Donate Now </h2>
+             <p> You help the biggest divers community growth! </p>
 
+         </div>
 
     </div>
 );
