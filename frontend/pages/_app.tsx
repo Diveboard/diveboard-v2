7cb433ed7937be5304d2b11684270c424bf16777
Serghey Samokhval <samsonskiff@gmail.com>
2022-05-24 14:17:54 +0300

    fix: lint warnings, dive buddies, stripe wrapper

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,21 +1,15 @@
 import React from 'react';
-import { loadStripe } from '@stripe/stripe-js';
-import { Elements } from '@stripe/react-stripe-js';
 import { AuthCodeTimer } from '../src/layouts/AuthCodeTimer';
 import '../styles/globals.css';
-
-const stripePromise = loadStripe(`${process.env.NEXT_PUBLIC_STRIPE_TEST_PUBLISHABLE_KEY}`);
 
 function MyApp({
   Component,
   pageProps,
 }): JSX.Element {
   return (
-    <Elements stripe={stripePromise}>
-      <AuthCodeTimer>
-        <Component {...pageProps} />
-      </AuthCodeTimer>
-    </Elements>
+    <AuthCodeTimer>
+      <Component {...pageProps} />
+    </AuthCodeTimer>
   );
 }
 
