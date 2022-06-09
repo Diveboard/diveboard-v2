50049a05e73e180c6efbfb978dbe5c44399f585c
Serghey Samokhval <samsonskiff@gmail.com>
2022-06-09 14:12:10 +0300

    add: responsive about-us page

--- frontend/src/components/PageBlocks/AboutUsBlocks/index.tsx
+++ frontend/src/components/PageBlocks/AboutUsBlocks/index.tsx
@@ -6,19 +6,23 @@ import { KeyFeaturesBlock } from './KeyFeaturesBlock';
 import { CrewBlock } from './CrewBlock';
 import { AboutMembers } from './AboutMembers';
 import { OurPartners } from './OurParnters';
+import styles from './styles.module.scss';
 
 const AboutUsBlock = () => (
   <WrapperContainer>
     <AboutUsBanner />
-    <InfoTextBlock
-      imgSrc="/images/collaboration.png"
-      title="How it all began"
-      text={'It all began with a frustrated computer geek.\n'
-        + 'Alex had a Mares M2 dive computer and a (cheap) Digital Camera with an underwater housing. Nothing uncommon yet. But It was incredibly painful to do anything useful with the dive profiles on his computer. They were ending up as printed pdfs in a long-forgotten folder and were too ugly to post on Flickr. The pictures were a mess too. And Alex\'s handwriting was so poor that he really had trouble reading what was written down in his paper logbook.\n'
-        + 'From all this frustration shared with Pascal and Richard we got to building Diveboard. We started it as a side project which grew pretty much to become Alex\'s main occupation about 18hrs a day (yes he gets easily obsessed).\n'
-        + 'Diveboard\'s objective is fairly straightforward: become the place for scuba divers to keep and share their diving memories. Also we want Diveboard to be a place to help scuba divers discover new places, species and fellow divers to make their diving experience even more enjoyable.\n'
-        + 'While still under the radar - Diveboard opened silently the gates on April 8th after a few months of preparation. '}
-    />
+    <div className={styles.wrapper}>
+      <InfoTextBlock
+        imgSrc="/images/how-it-began.png"
+        title="How it all began"
+        text={'It all began with a frustrated computer geek.\n'
+              + 'Alex had a Mares M2 dive computer and a (cheap) Digital Camera with an underwater housing. Nothing uncommon yet. But It was incredibly painful to do anything useful with the dive profiles on his computer. They were ending up as printed pdfs in a long-forgotten folder and were too ugly to post on Flickr. The pictures were a mess too. And Alex\'s handwriting was so poor that he really had trouble reading what was written down in his paper logbook.\n'
+              + 'From all this frustration shared with Pascal and Richard we got to building Diveboard. We started it as a side project which grew pretty much to become Alex\'s main occupation about 18hrs a day (yes he gets easily obsessed).\n'
+              + 'Diveboard\'s objective is fairly straightforward: become the place for scuba divers to keep and share their diving memories. Also we want Diveboard to be a place to help scuba divers discover new places, species and fellow divers to make their diving experience even more enjoyable.\n'
+              + 'While still under the radar - Diveboard opened silently the gates on April 8th after a few months of preparation. '}
+      />
+    </div>
+
     <KeyFeaturesBlock />
     <CrewBlock />
     <AboutMembers />
