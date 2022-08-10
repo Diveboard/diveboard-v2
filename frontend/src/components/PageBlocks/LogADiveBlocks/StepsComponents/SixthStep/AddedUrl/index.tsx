d18b98d9f9ddc9619d501813b64001d97a76af5d
Serghey Samokhval <samsonskiff@gmail.com>
2022-08-10 13:19:59 +0300

    add: sixth step

---
+++ frontend/src/components/PageBlocks/LogADiveBlocks/StepsComponents/SixthStep/AddedUrl/index.tsx
@@ -0,0 +1,22 @@
+import React, { FC } from 'react';
+import { Icon } from '../../../../../Icons/Icon';
+import styles from './styles.module.scss';
+
+type Props = {
+  url: string
+  setMediaUrl: React.Dispatch<React.SetStateAction<string[]>>
+};
+
+export const AddedUrl: FC<Props> = ({ url, setMediaUrl }) => {
+  const deleteUrlHandler = () => {
+    setMediaUrl((prevUrls) => prevUrls.filter((item) => item !== url));
+  };
+  return (
+    <span className={styles.urlItem}>
+      <span className={styles.urlName}>{url}</span>
+      <span onClick={deleteUrlHandler} className={styles.delete}>
+        <Icon iconName="close" width={12} height={12} />
+      </span>
+    </span>
+  );
+};
