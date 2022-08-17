7d8f788aa5c3cf75955b9a890e3c649ddb90383e
Serghey Samokhval <samsonskiff@gmail.com>
2022-08-17 11:27:10 +0300

    fix: Input component style

--- frontend/pages/auth.tsx
+++ frontend/pages/auth.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { GetServerSideProps, NextPage } from 'next';
+import { NextPage } from 'next';
 import { SignInBlock } from '../src/components/PageBlocks/SignInBlock';
 import { AuthLayout } from '../src/layouts/AuthLayout';
 import { MainLayout } from '../src/layouts/MainLayout';
-import { firebaseAdmin } from '../src/firebase/firebaseAdmin';
 
 const Auth: NextPage = () => (
   <AuthLayout>
@@ -12,32 +11,5 @@ const Auth: NextPage = () => (
     </MainLayout>
   </AuthLayout>
 );
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const uid = context.req.cookies.__session;
-
-  if (!uid) {
-    return {
-      props: {
-        user: null,
-      },
-    };
-  }
-
-  const {
-    email, photoURL = '', displayName = '',
-  } = await firebaseAdmin.auth().getUser(uid);
-
-  return {
-    props: {
-      user: {
-        uid,
-        email,
-        photoURL,
-        name: displayName,
-      },
-    },
-  };
-};
 
 export default Auth;
