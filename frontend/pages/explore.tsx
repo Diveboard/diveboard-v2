2c5124f7e2441ef2205a9b725bb3422e2be2156a
Fenomen-Alex <alexfenomen@gmail.com>
2022-09-21 12:43:54 +0300

    explore page - fix

--- frontend/pages/explore.tsx
+++ frontend/pages/explore.tsx
@@ -2,17 +2,23 @@ import React from 'react';
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import pageRoutes from '../src/routes/pagesRoutes.json';
 import { firebaseAdmin } from '../src/firebase/firebaseAdmin';
-import { MainLayout } from '../src/layouts/MainLayout';
 import { AuthLayout } from '../src/layouts/AuthLayout';
 import ExploreBlock from '../src/components/PageBlocks/ExploreBlock';
+import { useWindowWidth } from '../src/hooks/useWindowWidth';
+import { UserHeader } from '../src/components/Header/DesktopHeader';
+import { Footer } from '../src/components/Footer/DesktopFooter';
+import { MobileNavBar } from '../src/components/MobileNavBar';
 
-const Explore: InferGetServerSidePropsType<typeof getServerSideProps> = ({ user }) => (
-  <AuthLayout user={user}>
-    <MainLayout>
-      <ExploreBlock />
-    </MainLayout>
-  </AuthLayout>
-);
+const Explore: InferGetServerSidePropsType<typeof getServerSideProps> = ({ user }) => {
+  const isMobile = useWindowWidth(500, 769);
+  return (
+    <AuthLayout user={user}>
+      {!isMobile && <UserHeader />}
+      <ExploreBlock isMobile={isMobile} />
+      {!isMobile ? <Footer /> : <MobileNavBar loggedIn={!!user} />}
+    </AuthLayout>
+  );
+};
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const uid = context.req.cookies.__session;
