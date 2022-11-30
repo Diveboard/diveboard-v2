66ca6085702c7db32a5ff1c864751a83e379fb47
StasSytnykov <stas82kak05@gmail.com>
2022-11-30 11:47:32 +0200

    added mocked images

--- frontend/src/components/PageBlocks/GalleryBlock/index.tsx
+++ frontend/src/components/PageBlocks/GalleryBlock/index.tsx
@@ -17,6 +17,57 @@ export const GalleryBlock = ({ images, user }: Props) => {
   const [openLightbox, setOpenLightbox] = useState(false);
   const [imageIndex, setImageIndex] = useState<number>(null);
   const [sortType, setSortType] = useState('recent');
+  const mockImages = [
+    {
+      img: 'https://burst.shopifycdn.com/photos/person-holds-a-book-over-a-stack-and-turns-the-page.jpg?width=1200&format=pjpg&exif=0&iptc=0',
+      date: null,
+      draft: true,
+      spot: 'blalba',
+    },
+    {
+      img: 'https://helios-i.mashable.com/imagery/articles/02mlaG4zdU1P0uiPF4NFzHK/hero-image.fill.size_1200x900.v1657640165.jpg',
+      date: null,
+      draft: true,
+      spot: 'blalba',
+    },
+    {
+      img: 'https://cdn.pixabay.com/photo/2018/06/17/20/35/chain-3481377_960_720.jpg',
+      date: null,
+      draft: true,
+      spot: 'blalba',
+    },
+    {
+      img: 'https://cdn.vox-cdn.com/thumbor/M_r5xylDOEVXpor4QkEt1bky63I=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/22863764/33433274343_277deba9eb_o.jpg',
+      date: null,
+      draft: true,
+      spot: 'blalba',
+    },
+    {
+      img: 'https://www.esa.int/var/esa/storage/images/esa_multimedia/images/2021/03/artist_s_impression_of_the_james_webb_space_telescope/23231075-5-eng-GB/Artist_s_impression_of_the_James_Webb_Space_Telescope_pillars.jpg',
+      date: null,
+      draft: true,
+      spot: 'blalba',
+    },
+    {
+      img: 'https://cdn.pixabay.com/photo/2022/09/03/10/52/northern-gannets-7429282_960_720.jpg',
+      date: null,
+      draft: true,
+      spot: 'blalba',
+    },
+    {
+      img: 'https://cdn.pixabay.com/photo/2022/08/25/09/35/germany-7409782_960_720.jpg',
+      date: null,
+      draft: true,
+      spot: 'blalba',
+    },
+    {
+      img: 'https://cdn.pixabay.com/photo/2022/10/28/08/25/amethyst-7552587_960_720.jpg',
+      date: null,
+      draft: true,
+      spot: 'blalba',
+    },
+  ];
+
   useEffect(() => {
     document.body.style.overflow = 'overlay';
   }, []);
@@ -58,7 +109,7 @@ export const GalleryBlock = ({ images, user }: Props) => {
       <div
         className={styles.imageGrid}
       >
-        {filterBySearch(sortImages(images)).map((photo, index) => (
+        {filterBySearch(sortImages(mockImages)).map((photo, index) => (
           <div
               /* eslint-disable-next-line react/no-array-index-key */
             key={index}
@@ -77,12 +128,12 @@ export const GalleryBlock = ({ images, user }: Props) => {
       </div>
       <Lightbox
         open={openLightbox}
-        image={images[imageIndex]}
+        image={mockImages[imageIndex]}
         user={user}
         onClose={() => {
           setOpenLightbox(false);
         }}
-        handleNextSlide={() => (imageIndex < images.length - 1
+        handleNextSlide={() => (imageIndex < mockImages.length - 1
           ? setImageIndex((idx) => idx + 1)
           : setOpenLightbox(false))}
         handlePrevSlide={() => (imageIndex > 0
