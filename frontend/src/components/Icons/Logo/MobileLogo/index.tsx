3441389081a207028c7796d7b868b0247286797b
Liza Babych <bettyhetfield11951@gmail.com>
2023-02-03 10:18:41 +0200

    fix

--- frontend/src/components/Icons/Logo/MobileLogo/index.tsx
+++ frontend/src/components/Icons/Logo/MobileLogo/index.tsx
@@ -1,8 +1,9 @@
-import React, { FC } from 'react';
+import React, { FC, useContext } from 'react';
 import Link from 'next/link';
 import { Icon } from '../../Icon';
 import pagesRoutes from '../../../../routes/pagesRoutes.json';
 import styles from './styles.module.scss';
+import { AuthStatusContext } from '../../../../layouts/AuthLayout';
 
 type Props = {
   filled?: boolean;
@@ -10,9 +11,12 @@ type Props = {
 
 export const LogoMobile: FC<Props> = ({ filled = true }) => {
   const logoStyle = filled ? styles.filled : styles.notFilled;
+  const {
+    userAuth,
+  } = useContext(AuthStatusContext);
   return (
     <div className={styles.logo}>
-      <Link href={pagesRoutes.mainPageRoute}>
+      <Link href={userAuth?.uid ? `/logbook/${userAuth.uid}` : pagesRoutes.mainPageRoute}>
         <a className={logoStyle}>
           <Icon iconName="logo" width={120} height={17} />
         </a>
