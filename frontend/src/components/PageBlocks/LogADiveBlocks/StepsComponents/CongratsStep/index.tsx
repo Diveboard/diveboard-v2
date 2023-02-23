8f0f24ed71a596f67027f8245a11e858e3bddd6f
Liza Babych <bettyhetfield11951@gmail.com>
2023-02-23 20:24:40 +0200

    fix

--- frontend/src/components/PageBlocks/LogADiveBlocks/StepsComponents/CongratsStep/index.tsx
+++ frontend/src/components/PageBlocks/LogADiveBlocks/StepsComponents/CongratsStep/index.tsx
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useRouter } from 'next/router';
 import styles from './styles.module.scss';
-import { ContentModeType, PlanType } from '../../../DonateBlocks/donateTypes';
 import { Icon } from '../../../../Icons/Icon';
 import { Button } from '../../../../Buttons/Button';
 
