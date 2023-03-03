32e4e510a39317b6549ca14e8eda7347f4655d96
Liza Babych <bettyhetfield11951@gmail.com>
2023-03-03 13:45:31 +0200

    fixes

--- frontend/src/utils/refreshCache.ts
+++ frontend/src/utils/refreshCache.ts
@@ -4,6 +4,14 @@ export const deleteCache = async () => {
   if ('serviceWorker' in navigator) {
     const wb: Workbox = new Workbox('./sw.js');
     await caches.delete('next-data');
+    wb.register();
+  }
+};
+
+export const refreshCache = async () => {
+  if ('serviceWorker' in navigator) {
+    const wb: Workbox = new Workbox('./sw.js');
+    await caches.delete('next-data');
     await caches.delete('others');
     wb.register();
   }
